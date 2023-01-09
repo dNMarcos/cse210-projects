2da5868801f59e4c7240b8c4d25ad5529256f2ef
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-09 00:45:11 -0600

    Console.WriteLine(plsormin); erased

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string pgrade = Console.ReadLine();
+        int perg = int.Parse(pgrade);
+        string letter = "";
+        if (perg >= 90)
+        {
+            letter = "A";
+        }
+        else if (perg >= 80)
+        {
+            letter = "B";
+        }
+        else if (perg >= 70)
+        {
+            letter = "C";
+        }
+        else if (perg >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int plsormin = perg%10;
+        string letter2 = "";
+        if (plsormin >= 7 && letter != "A")
+        {
+            letter2 = "+";
+        }
+        else if (plsormin <= 3 && letter != "F")
+        {
+            letter2 = "-";
+        }
+        else
+        {
+            letter2 = "";
+        }
+        Console.WriteLine($"Your grade is: {letter}{letter2}.");
+
+        if (perg >= 70)
+        {
+            Console.WriteLine("Congratulation, you pass the course! ");
+        }
+        else
+        {
+            Console.WriteLine("You fail the course, please take it again. ");
+        }
+
     }
 }
