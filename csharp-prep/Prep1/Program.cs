8f34513bef6ea79f2857d668ed3d2aec9b3d885c
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-08 23:08:10 -0600

    The name was inserted

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is your second name? ");
+        string sname = Console.ReadLine();
+        Console.WriteLine($"Your name is {sname}, {fname} {sname}.");
     }
 }
