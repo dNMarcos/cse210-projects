4add1467a46dec8ff47a22f8b00e643e2c3653bf
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-11 22:34:07 -0600

    2nd question added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,5 +7,9 @@ class Program
         Console.WriteLine("What is the magic number?");
         string magi_n = Console.ReadLine();
         int maginumb = int.Parse(magi_n);
+
+        Console.WriteLine("What is your guess?");
+        int guess_n = int.Parse(Console.ReadLine());
+        
     }
 }
