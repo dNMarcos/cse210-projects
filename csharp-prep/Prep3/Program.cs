8d4cd19f29d70a51fbb3019d05441d7e487ce97b
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-12 01:08:20 -0600

    while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,19 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int mn = randomGenerator.Next(1,100);
-        int count = 0;
         int gn = 0;
-        string ans = "";
-        while (ans == "yes")
+        string answ = " ";
+        while (answ == "yes")
         {
+            Random randomGenerator = new Random();
+            int mn = randomGenerator.Next(1,100);
             while (gn != mn)
             {
                 Console.WriteLine("what is your guess?");
                 string guess = Console.ReadLine();
                 gn = int.Parse(guess);
-
+                
                 if (gn < mn)
                 {
                     Console.WriteLine("Higher");
@@ -29,11 +28,9 @@ class Program
                 {
                     Console.WriteLine("Congratulation");
                 }
-                count++;
-                Console.WriteLine(count);
             }
             Console.WriteLine("Do you want to continue?");
-            ans = Console.ReadLine();
+            answ = Console.ReadLine();
         }
     }
 }
