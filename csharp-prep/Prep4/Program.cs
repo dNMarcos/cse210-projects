911cb63cc57615d5f2ab2fc471e36f34407f1a15
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-15 01:12:23 -0600

    last function

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a List of number, type 0 when finished.");
+        List<int> game = new List<int>();
+        
+        int usernumb = 1;
+        while(usernumb != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            usernumb = int.Parse(Console.ReadLine());
+            game.Add(usernumb);
+        }
+
+        int sum = 0;
+        foreach(int numbers in game)
+        {
+            sum += numbers;
+        }
+
+        Console.WriteLine($" The sum is: {sum}");
+        float total = Convert.ToSingle(sum) / game.Count;
+        Console.WriteLine($" The average is: {total}");
+
+        Console.WriteLine($"The largest number is: {game.Max()}");
+
+        int minp = game[0];
+        foreach(int minumb in game)
+        {
+            if((minumb < minp) && (minumb > 0))
+            {
+                minp = minumb;
+                Console.WriteLine($"The smallest positive number is: {minp}");
+            }
+        }
+
+        List<int> games = game.OrderBy(number2 => number2).ToList();
+        foreach(int numbers in games)
+        {
+            Console.WriteLine($"The sorted list is: {numbers}");
+        } 
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int UserNumber = PromtUserNumber();
+
+        int SquaredNumber = SquareNumber(UserNumber);
+        DisplayResult(UserName, SquaredNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromtUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sq = number * number;
+            return sq;
+        }
+
+        static void DisplayResult(string name, int sq)
+        {
+            Console.WriteLine($"{name}, the square of your number is {sq}");
+        }
     }
 }
