1a8f85a92ca7bb9a8791864b792257db98ed3ed3
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-22 01:57:17 -0600

    Before of create the member variable, create the method or function to acces the variable

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,12 +1,15 @@
 using System;
-
-//git commit -m "Create the member variable for the person's name" prepare/Learning02/Resume.cs
+//git add prepare/Learning02/Program.cs
+//git commit -m "Create the member variable for the list of Jobs" prepare/Learning02/Resume.cs
 //prepare/Learning02/Job.cs
 //prepare/Learning02/Progra.cs
 public class Resume
 {
     public string _personName = "";
     public List<Job> _lJobs = new List<Job>();
+    Console.WriteLine($"Name: {_personName}");
+    Console.WriteLine($"Jobs: {_lJobs}");
+
 
 
 
