7c55a69e5b94e436a09bff37a98bc85bb597103d
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-22 01:36:21 -0600

    Create the Resume class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,19 @@
+
+using System;
+// A code template for the category of things known as Job. The
+// responsibility of a Person is to hold and display the Job information
+public class Job
+{
+    // The C# convention is to start member variables with an underscore _
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear;
+    public int _endYear;
+    public void jobsDisp()
+    {
+    Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._jobTitle ="Software Engineer";
+        job1._company ="Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2002;
+        job2._jobTitle = "Manager";
+        job2._company ="Apple";
+        job2._startYear = 1999;
+        job2._endYear = 2005;
+        job1.jobsDisp();
+        job2.jobsDisp();
+
+
+        /*Console.WriteLine("Hello Learning02 World!");*/
     }
 }
