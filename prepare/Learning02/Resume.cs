27ace2a36e8faffbf48e611234e4078bd7b3f332
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-01-22 01:48:54 -0600

    Create the member variable for the list of Jobs

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,8 +1,13 @@
 using System;
 
+//git commit -m "Create the member variable for the person's name" prepare/Learning02/Resume.cs
+//prepare/Learning02/Job.cs
+//prepare/Learning02/Progra.cs
 public class Resume
 {
     public string _personName = "";
+    public List<Job> _lJobs = new List<Job>();
+
 
 
 
