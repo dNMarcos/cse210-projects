9aedb0227d59aacc3992903974ca18bf59588151
DianaLMN <47658266+DianaLMN@users.noreply.github.com>
2023-02-03 04:25:21 -0600

    Results

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -9,17 +9,31 @@ public class Fraction
     {
         _top = 1;
         _bottom = 1;
-        
+
     }
 
     public Fraction(int top)
     {
+        _top = top;
         _bottom = 1;
     }
 
-    private Fraction(int top, int bottom)
+    public Fraction(int top, int bottom)
     {
+        _top = top;
+        _bottom = bottom;
+    }
 
+    public string GetFractionString()
+    {
+        string frac = _top + "/" + _bottom;
+        return frac;
+    }
+
+    public double GetDecimalValue()
+    {
+        double fract_d = Convert.ToDouble(_top) / Convert.ToDouble(_bottom);
+        return fract_d;
     }
 
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,7 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Fraction f = new Fraction();
-    
+        Console.WriteLine(f.GetFractionString());
+
+        Fraction fd = new Fraction();
+        Console.WriteLine(fd.GetDecimalValue());
+
+        Fraction ft = new Fraction(5);
+        Console.WriteLine(ft.GetFractionString());
+        Console.WriteLine(ft.GetDecimalValue());
+
+        Fraction fth = new Fraction(3,4);
+        Console.WriteLine(fth.GetFractionString());
+        Console.WriteLine(fth.GetDecimalValue());
+
+        Fraction fon = new Fraction(1,3);
+        Console.WriteLine(fon.GetFractionString());
+        Console.WriteLine(fon.GetDecimalValue());    
         
         /*Console.WriteLine("Hello Learning003 World!");*/
     }
